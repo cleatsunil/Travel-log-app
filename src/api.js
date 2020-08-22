96b10a0425d076ba60af21905181eda4ab21ded7
Cleatus sunil <sunil@dmarktechnologies.com>
2020-08-22 09:49:01 +0530

    cors

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
-const API_URL =
-  window.location.hostname === 'localhost'
-    ? 'https://localhost:1337'
-    : 'https://cleat-travel-log-app.herokuapp.com';
+// const API_URL =
+//   window.location.hostname === 'localhost'
+//     ? 'https://localhost:1337'
+//     : 'https://cleat-travel-log-app.herokuapp.com';
+
+const API_URL = 'https://cleat-travel-log-app.herokuapp.com';
 
 export async function listLogEntries() {
   const response = fetch(`${API_URL}/api/logs`);
