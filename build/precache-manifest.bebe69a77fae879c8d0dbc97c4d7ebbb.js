49e466da32b4d7b0e5ea19b6707d9e5cdf3597f8
Cleatus sunil <sunil@dmarktechnologies.com>
2020-08-22 07:50:48 +0530

    Changed the api.js

---
+++ build/precache-manifest.bebe69a77fae879c8d0dbc97c4d7ebbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4632eb575dab312f9c05476a3ce7ad4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9566eaa37729e5cd565b",
+    "url": "/static/css/main.e6467def.chunk.css"
+  },
+  {
+    "revision": "f398911aa0c23dacdce8",
+    "url": "/static/js/2.498e732c.chunk.js"
+  },
+  {
+    "revision": "61eedd39499d34d7aaf09803c460ef46",
+    "url": "/static/js/2.498e732c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9566eaa37729e5cd565b",
+    "url": "/static/js/main.9edda6f6.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
